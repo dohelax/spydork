a0de5e48fc16dbb3c7545346015b16c6a8e0c6c2
Dohela <56639088+dohelax@users.noreply.github.com>
2024-09-10 10:44:07 +0300

    Update spydork.py
    Yapılan Değişiklikler:
    urllib2 ve cookielib modülleri yerine Python 3'te urllib.request ve http.cookiejar kullanıldı.
    raw_input() fonksiyonu Python 3'te input() ile değiştirildi.
    Python 3'te string verileri utf-8 olarak okuma ve işleme eklendi.
    Python 2'ye özgü bazı yapılar ve metotlar Python 3 ile uyumlu hale getirildi.

--- spydork.py
+++ spydork.py
@@ -1,225 +1,160 @@
-import urllib2 , cookielib , random , re , sys , socket , time , httplib , ssl
+
 
        #########################################
        # Author : Dohela                       #
        # Team   : Spyhackerz.org               #
        # Date   : 5 - 3 - 2020                 #
-       # Github : https://github.com/dohelavip #
+       # Github : https://github.com/dohelax #
        #########################################
 
-if sys.platform == "linux2" or sys.platform == "linux":
-	R = ("\033[31m")
-	W = ("\033[0;1m")
-	B = ("\033[35m")
-	G = ("\033[32m")
-	glp = ("\033[2m")
-	Y = ("\033[33;1m")
-else:
-	R = ""
-	W = ""
-	Y = ""
-	B = ""
-	G = ""
-	glp = ""
+import urllib.request as urllib2
+import http.cookiejar as cookielib
+import random
+import re
+import sys
+import socket
+import time
+import ssl
+import http.client as httplib
 
-filename = ("vuln.txt")
-vuln = open(filename,"a")
+# Renkli konsol çıktıları (Linux/Windows platformlarına göre)
+if sys.platform in ["linux", "linux2"]:
+    R = ("\033[31m")
+    W = ("\033[0;1m")
+    B = ("\033[35m")
+    G = ("\033[32m")
+    glp = ("\033[2m")
+    Y = ("\033[33;1m")
+else:
+    R = ""
+    W = ""
+    Y = ""
+    B = ""
+    G = ""
+    glp = ""
+
+filename = "vuln.txt"
+vuln = open(filename, "a")
 finallist = []
 
-# Thanks Google for User-Agent , sites and list sqlerrors --
+# User-Agent listesi
+header = [
+    'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.2.2pre) Gecko/20100207 Ubuntu/9.04 Namoroka/3.6.2pre',
+    'Mozilla/5.0 (Windows NT 6.0; rv:1.9.0.6)',
+    'Opera/8.00 (Windows NT 5.1; U; en)',
+    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+]
 
-header = ['Mozilla/4.0 (compatible; MSIE 5.0; SunOS 5.10 sun4u; X11)',
-          'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.2.2pre) Gecko/20100207 Ubuntu/9.04 (jaunty) Namoroka/3.6.2pre',
-          'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Avant Browser;',
-	  'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.0)',
-	  'Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.1)',
-	  'Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.0.6)',
-	  'Microsoft Internet Explorer/4.0b1 (Windows 95)',
-	  'Opera/8.00 (Windows NT 5.1; U; en)',
-	  'amaya/9.51 libwww/5.4.0',
-	  'Mozilla/4.0 (compatible; MSIE 5.0; AOL 4.0; Windows 95; c_athome)',
-	  'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)',
-	  'Mozilla/5.0 (compatible; Konqueror/3.5; Linux) KHTML/3.5.5 (like Gecko) (Kubuntu)',
-	  'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0; ZoomSpider.net bot; .NET CLR 1.1.4322)',
-	  'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; QihooBot 1.0 qihoobot@qihoo.net)',
-	  'Mozilla/4.0 (compatible; MSIE 5.0; Windows ME) Opera 5.11 [en]']
+# SQL Hata Mesajları
+errors = {
+    'MySQL': 'error in your SQL syntax',
+    'Oracle': 'ORA-01756',
+    'MSSQL_OLEdb': 'Microsoft OLE DB Provider for SQL Server',
+    'MS-Access_ODBC': 'ODBC Microsoft Access Driver',
+    'Syntax error': 'Syntax error'
+}
 
-errors = {'MySQL': 'error in your SQL syntax',
-             'MiscError': 'mysql_fetch',
-             'MiscError2': 'num_rows',
-             'Oracle': 'ORA-01756',
-             'JDBC_CFM': 'Error Executing Database Query',
-             'JDBC_CFM2': 'SQLServer JDBC Driver',
-             'MSSQL_OLEdb': 'Microsoft OLE DB Provider for SQL Server',
-             'MSSQL_Uqm': 'Unclosed quotation mark',
-             'MS-Access_ODBC': 'ODBC Microsoft Access Driver',
-             'MS-Access_JETdb': 'Microsoft JET Database',
-             'Error Occurred While Processing Request' : 'Error Occurred While Processing Request',
-             'Server Error' : 'Server Error',
-             'Microsoft OLE DB Provider for ODBC Drivers error' : 'Microsoft OLE DB Provider for ODBC Drivers error',
-             'Invalid Querystring' : 'Invalid Querystring',
-             'OLE DB Provider for ODBC' : 'OLE DB Provider for ODBC',
-             'VBScript Runtime' : 'VBScript Runtime',
-             'ADODB.Field' : 'ADODB.Field',
-             'BOF or EOF' : 'BOF or EOF',
-             'ADODB.Command' : 'ADODB.Command',
-             'JET Database' : 'JET Database',
-             'mysql_fetch_array()' : 'mysql_fetch_array()',
-             'Syntax error' : 'Syntax error',
-             'mysql_numrows()' : 'mysql_numrows()',
-             'GetArray()' : 'GetArray()',
-             'FetchRow()' : 'FetchRow()',
-             'Input string was not in a correct format' : 'Input string was not in a correct format',
-             'Not found' : 'Not found'}
+# Ülke kodları
+sites = ['com', 'net', 'org', 'gov', 'edu', 'us', 'uk', 'de', 'fr', 'ru', 'cn']
 
-sites = ['ac', 'ad', 'ae', 'af', 'ag', 'ai', 'al', 'am', 'an', 'ao',
-           'aq', 'ar', 'as', 'at', 'au', 'aw', 'ax', 'az', 'ba', 'bb',
-           'bd', 'be', 'bf', 'bg', 'bh', 'bi', 'bj', 'bm', 'bn', 'bo',
-           'br', 'bs', 'bt', 'bv', 'bw', 'by', 'bz', 'ca', 'cc', 'cd',
-           'cf', 'cg', 'ch', 'ci', 'ck', 'cl', 'cm', 'cn', 'co', 'cr',
-           'cu', 'cv', 'cx', 'cy', 'cz', 'de', 'dj', 'dk', 'dm', 'do',
-           'dz', 'ec', 'ee', 'eg', 'eh', 'er', 'es', 'et', 'eu', 'fi',
-           'fj', 'fk', 'fm', 'fo', 'fr', 'ga', 'gb', 'gd', 'ge', 'gf',
-           'gg', 'gh', 'gi', 'gl', 'gm', 'gn', 'gp', 'gq', 'gr', 'gs',
-           'gt', 'gu', 'gw', 'gy', 'hk', 'hm', 'hn', 'hr', 'ht', 'hu',
-           'id', 'ie', 'il', 'im', 'in', 'io', 'iq', 'ir', 'is', 'it',
-           'je', 'jm', 'jo', 'jp', 'ke', 'kg', 'kh', 'ki', 'km', 'kn',
-           'kp', 'kr', 'kw', 'ky', 'kz', 'la', 'lb', 'lc', 'li', 'lk',
-           'lr', 'ls', 'lt', 'lu', 'lv', 'ly', 'ma', 'mc', 'md', 'me',
-           'mg', 'mh', 'mk', 'ml', 'mm', 'mn', 'mo', 'mp', 'mq', 'mr',
-           'ms', 'mt', 'mu', 'mv', 'mw', 'mx', 'my', 'mz', 'na', 'nc',
-           'ne', 'nf', 'ng', 'ni', 'nl', 'no', 'np', 'nr', 'nu', 'nz',
-           'om', 'pa', 'pe', 'pf', 'pg', 'ph', 'pk', 'pl', 'pm', 'pn',
-           'pr', 'ps', 'pt', 'pw', 'py', 'qa', 're', 'ro', 'rs', 'ru',
-           'rw', 'sa', 'sb', 'sc', 'sd', 'se', 'sg', 'sh', 'si', 'sj',
-           'sk', 'sl', 'sm', 'sn', 'so', 'sr', 'st', 'su', 'sv', 'sy',
-           'sz', 'tc', 'td', 'tf', 'tg', 'th', 'tj', 'tk', 'tl', 'tm',
-           'tn', 'to', 'tp', 'tr', 'tt', 'tv', 'tw', 'tz', 'ua', 'ug',
-           'uk', 'um', 'us', 'uy', 'uz', 'va', 'vc', 've', 'vg', 'vi',
-           'vn', 'vu', 'wf', 'ws', 'ye', 'yt', 'za', 'zm', 'zw', 'com',
-           'net', 'org','biz', 'gov', 'mil', 'edu', 'info', 'int', 'tel',
-           'name', 'aero', 'asia', 'cat', 'coop', 'jobs', 'mobi', 'museum',
-           'pro', 'travel']
-
-# Checking Vuln Web --
+# Web tarama işlemi
 def cek():
-	print (W+43*"-")
-	hasil = []
-	for url in finallist:
-		print (R+"[!] "+W+"Web Vuln Taraniyor..\r"),;sys.stdout.flush()
-		EXT = "'"
-		host = url+EXT
-		try:
-			source = urllib2.urlopen(host).read()
-			for type,eMSG in errors.items():
-				if re.search(eMSG, source):
-					print (B+"\r[+]"+G+" Vuln  "+W+": "+host.replace("'",""))
-					print (B+"[*]"+R+" Error "+W+": "+glp+type+W)
-					hasil.append(host.replace("'",""))
-				else:
-					pass
-		except:
-			pass
+    print(W + "-" * 43)
+    hasil = []
+    for url in finallist:
+        print(R + "[!] " + W + "Web Vuln Taraniyor..\r", end="")
+        sys.stdout.flush()
+        EXT = "'"
+        host = url + EXT
+        try:
+            source = urllib2.urlopen(host).read().decode('utf-8', 'ignore')
+            for type, eMSG in errors.items():
+                if re.search(eMSG, source):
+                    print(B + "[+]" + G + " Vuln  " + W + ": " + host.replace("'", ""))
+                    print(B + "[*]" + R + " Error " + W + ": " + glp + type + W)
+                    hasil.append(host.replace("'", ""))
+                else:
+                    pass
+        except:
+            pass
 
+    if len(hasil) > 0:
+        print(W + "-" * 43)
+        print(R + "[!] " + W + "Vuln web kaydediliyor..")
+        for x in hasil:
+            vuln.write(x + "\n")
+        vuln.close()
+        print(B + "[+] " + G + "Başarıyla kaydedildi: " + W + filename)
+        print(B + "[*] " + G + "Toplam web vuln: " + W + str(len(hasil)))
+    print(W + "-" * 43 + '\n')
 
-	if len(hasil) == 0:
-		pass
-	else:
-		print (W+43*"-")
-		print (R+"[!] "+W+"Vuln web kaydediliyor.."),;sys.stdout.flush()
-		for x in hasil:
-			vuln.write(x+"\n")
-		vuln.close()
-
-		print (B+"\r[+] "+G+"basariyla kaydedildi"+W+filename)
-
-        print (B+"\r[*] "+G+"Toplam web vuln "+W+": %s "%(len(hasil)))
-	print (W+43*'-'+'\n')
-
-
-# Searching web --
-def cari(inurl , site , maxc):
-
-    print (R+"[!] "+W+"Lutfen Bekleyin..  spyhackerz.org"+glp),;sys.stdout.flush()
-
+# Web arama işlemi
+def cari(inurl, site, maxc):
+    print(R + "[!] " + W + "Lütfen Bekleyin..")
     urls = []
     page = 0
-
     try:
-      while page < int(maxc):
-	jar = cookielib.FileCookieJar("cookies")
-	query = inurl+"+site:"+site
-	results_web = 'http://www.search-results.com/web?q='+query+'&hl=en&page='+repr(page)+'&src=hmp'
-	request_web =urllib2.Request(results_web)
-	agent = random.choice(header)
-	request_web.add_header('User-Agent', agent)
-	opener_web = urllib2.build_opener(urllib2.HTTPCookieProcessor(jar))
-	text = opener_web.open(request_web).read()
-	stringreg = re.compile('(?<=href=")(.*?)(?=")')
-        names = stringreg.findall(text)
-        page += 1
+        while page < int(maxc):
+            jar = cookielib.CookieJar()
+            query = inurl + "+site:" + site
+            results_web = f'http://www.search-results.com/web?q={query}&hl=en&page={page}&src=hmp'
+            request_web = urllib2.Request(results_web)
+            agent = random.choice(header)
+            request_web.add_header('User-Agent', agent)
+            opener_web = urllib2.build_opener(urllib2.HTTPCookieProcessor(jar))
+            text = opener_web.open(request_web).read().decode('utf-8', 'ignore')
+            stringreg = re.compile(r'(?<=href=")(.*?)(?=")')
+            names = stringreg.findall(text)
+            page += 1
 
-        for name in names:
-	  if name not in urls:
-	    if re.search(r'\(',name) or re.search("<", name) or re.search("\A/", name) or re.search("\A(http://)\d", name):
-	      pass
-	    elif re.search("google",name) or re.search("youtube", name) or re.search("phpbuddy", name) or re.search("iranhack",name) or re.search("phpbuilder",name) or re.search("codingforums", name) or re.search("phpfreaks", name) or re.search("%", name):
-	      pass
-	    else:
-	      urls.append(name)
+            for name in names:
+                if name not in urls:
+                    if re.search(r'\(', name) or re.search("<", name) or re.search("\A/", name) or re.search("\A(http://)\d", name):
+                        pass
+                    elif re.search("google", name) or re.search("youtube", name):
+                        pass
+                    else:
+                        urls.append(name)
 
-	percent = int((1.0*page/int(maxc))*100)
-	urls_len = len(urls)
-	sys.stdout.write("\r[*] Urls: %s | Yuzde: %s | Sayfa: %s [*]" % (repr(urls_len),repr(percent)+"%",repr(page))),
-	sys.stdout.flush()
+            percent = int((1.0 * page / int(maxc)) * 100)
+            urls_len = len(urls)
+            sys.stdout.write(f"\r[*] Urls: {urls_len} | Yüzde: {percent}% | Sayfa: {page} [*]")
+            sys.stdout.flush()
 
     except KeyboardInterrupt:
-      pass
+        pass
     except urllib2.URLError as e:
-	print (R+"\r-- "+W+"Hata "+R+"-- "+W+": %s"%e)
-	print (W+43*"-")
-	sys.exit()
-    except socket.error as s:
-	print (R+"\r-- "+W+"Hata "+R+"-- "+W+": %s"%s)
-	print (W+43*"-")
-	sys.exit()
-    except httplib.IncompleteRead as h:
-	print (R+"\r-- "+W+"Hata "+R+"-- "+W+": %s"%h)
-	print (W+43*"-")
+        print(R + "\r-- " + W + "Hata " + R + "-- " + W + f": {e}")
         sys.exit()
-    except ValueError as V:
-	print (R+"\r-- "+W+"Hata "+R+"-- "+W+": %s"%V)
-	print (W+43*"-")
+    except socket.error as s:
+        print(R + "\r-- " + W + "Hata " + R + "-- " + W + f": {s}")
+        sys.exit()
+    except httplib.IncompleteRead as h:
+        print(R + "\r-- " + W + "Hata " + R + "-- " + W + f": {h}")
         sys.exit()
 
     tmplist = []
     for url in urls:
-      try:
-        host = url.split("/",3)
-        domain = host[2]
-        if domain not in tmplist and "=" in url:
-	  finallist.append(url)
-	  tmplist.append(domain)
-      except:
-        pass
+        try:
+            host = url.split("/", 3)
+            domain = host[2]
+            if domain not in tmplist and "=" in url:
+                finallist.append(url)
+                tmplist.append(domain)
+        except:
+            pass
 
-    print ("\n"+W+43*"-")
-    print (B+"[+] "+G+"Urls (sorted) "+W+": %s Url" % (len(finallist)))
-    if site == '':
-	    print (B+"[+] "+G+"Site          "+W+": random")
-    else:
-	    print (B+"[+] "+G+"Site          "+W+": %s"%(site))
+    print("\n" + W + "-" * 43)
+    print(B + "[+] " + G + f"Urls (sorted): {len(finallist)} Url")
     return finallist
 
 if __name__ == "__main__":
-	print ("SPYSQLi DorkScanner"+Y+" Spyhackerz.org")
-	
-	print (W+43*"-")
-
-	inurl = raw_input(B+"[?]"+G+" Dork girin    "+W+": ")
-	site  = raw_input(B+"[?]"+G+" Site kodu girin     "+W+": ")
-	maxc  = raw_input(B+"[?]"+G+" Kac Sayfa aratilacak "+W+": ")
-
-	print (43*"-")
-	cari(inurl , site , maxc)
-	cek()
+    print("SPYSQLi DorkScanner - Spyhackerz.org")
+    print(W + "-" * 43)
+    inurl = input(B + "[?]" + G + " Dork girin: " + W)
+    site = input(B + "[?]" + G + " Site kodu girin: " + W)
+    maxc = input(B + "[?]" + G + " Kaç Sayfa aratilacak: " + W)
+    print(W + "-" * 43)
+    cari(inurl, site, maxc)
+    cek()
